4c40ff139bf04c1c46f584cca6998b272336a015
RiwsPy <thibault.debruyne@hotmail.fr>
2021-06-12 08:11:17 +0200

    version 2.0

--- app/static/js/papy.js
+++ app/static/js/papy.js
@@ -1,11 +1,17 @@
-let question = document.getElementById('questionBlock');
-let author = document.getElementById('author')
-let grandy = document.getElementById('grandy')
-let map = document.getElementById("map")
+var question = document.getElementById('questionBlock');
+var response = document.getElementById('responseBlock')
+var questionButton = document.getElementById("questionButton")
+var map = document.getElementById("map")
+var test_copy = document.getElementById("test_copy")
 
-function test(elt){
-    /*info.innerText = question.value;
-    const ul = document.getElementById('author');*/
+let attr_list = ['id', 'class', 'name', 'placeholder', 'autocomplete',
+'autofocus', 'required', 'rows', 'readonly', 'type', 'onClick', 'height', 'width']
+
+document.getElementById("GOOGLE_URL").src =
+    "https://maps.google.com/maps/api/js?key=AIzaSyDICnA0VqhMKNXJkwbZuWP26CMAedvYWVs&callback=initMap&libraries=&v=weekly"
+
+function test(){
+    // alert('coucou1')
     const url = 'http://127.0.0.1:5000/api/';
 
     var request = new Request(url, {
@@ -16,23 +22,53 @@ function test(elt){
 
     fetch(request)
     .then((resp) => resp.json())
-    .then(function(data) {
-        /*grandy.appendChild(document.createTextNode('Haha'));*/
-        let li = document.createElement('li');
-        let span = document.createElement('span');
-        author.appendChild(li)
-        li.appendChild(span)
-        span.innerText = data.formatted_address + ' (' + data.lat + ', ' + data.lng + ')'
-        if ( map !== null ){
+    .then((data) => {
+        // alert('coucou2')
+        response.style['display'] = 'inline-block';
+        questionButton.style['display'] = 'none';
+        response.innerHTML += data.papy_blabla
+
+        if ( map !== null & (data.lat != 0 | data.lng != 0)){
             map.style['height'] = '200px';
             initMap(data.lat, data.lng);}
-        /*return {'data': question.value}*/
+        
+        question = createNewBlock("textarea", question);
+        questionButton = createNewBlock("button", questionButton);
+        response = createNewBlock("textarea", response);
+        map = createNewBlock('div', map)
     })
     .catch(error => alert("Erreur : " + error));
 }
 
+
+function createNewBlock(cls, old_block) {
+    let new_block = document.createElement(cls)
+    for (let data of attr_list) {
+        if (old_block.hasAttribute(data)) {
+            new_block.setAttribute(data, old_block.getAttribute(data))
+        }
+    }
+    if (cls == 'textarea') {
+        old_block.setAttribute("readonly", "")
+    }
+    else if (cls == 'button') {
+        new_block.innerText = 'GO'
+    }
+
+    test_copy.appendChild(new_block)
+    return new_block
+}
+
+
 function initMap(latitude, longitude) {
-    new google.maps.Map(map, {
-        center: {lat: latitude, lng: longitude},
+    let location = {lat: latitude, lng: longitude};
+    let google_plan = new google.maps.Map(map, {
+        center: location,
         zoom: 13});
+
+    // google Marker
+    new google.maps.Marker({
+    	position: location,
+    	map: google_plan,
+    });
 }
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, render_template, jsonify
 from app.api import googlemaps, wikipedia
 from app.parsers import normalize
+from app.words import NO_POSITION, NO_WIKI, BEGIN_PHRASE, COMPLEMENT, NAME, FUNCTION, VERB
+from random import choice
 
 app = Flask(__name__)
 app.debug = True
@@ -24,22 +26,27 @@ def papy():
 @app.route('/api/', methods={'POST'})
 def api():
     # la recherche doit s'effectuer en appuyant sur entrée, non sur un bouton
+    print('lkala', request.form.get('questionBlock'))
     user_input = request.data.decode('utf-8')
+    print(user_input)
 
     maps_info = googlemaps.GoogleMaps()
     maps_info.req(normalize(user_input))
 
     wiki_info = wikipedia.Wiki().req(maps_info.lat, maps_info.lng)
     print(wiki_info.url)
-
-    return jsonify({
+    result = {
         'result': wiki_info.return_value,
         'lat': maps_info.lat,
         'lng': maps_info.lng,
         'formatted_address': maps_info.formatted_address,
         'wiki_url': wiki_info.url,
-        'blabla': wiki_info.info
-        }) # + console.log
+        'blabla': wiki_info.info[:100],
+        'papy_blabla': papy_response(maps_info, wiki_info)
+        }
+
+    print(result)
+    return jsonify(result) # + console.log
 
 @app.errorhandler(404)
 def page_404(error):
@@ -48,6 +55,16 @@ def page_404(error):
 @app.route('/animaux/')
 def animaux():
     return render_template('animaux.html')
+
+def papy_response(maps_info, wiki_info):
+    papy_text = \
+        choice(BEGIN_PHRASE) + ' ' + choice(NAME) + ' ' + \
+        choice(FUNCTION) + ' ' + choice(VERB) + ' ' + choice(COMPLEMENT)
+    if (maps_info.lat, maps_info.lng) == (0, 0):
+        return NO_POSITION
+    if wiki_info.url == '':
+        return NO_WIKI
+    return f"{papy_text}\r{maps_info.formatted_address} ? Voici le lien wiki, si tu souhaites en savoir davantage :\r{wiki_info.url}"
 
 if __name__ == "__main__":
     app.run()
